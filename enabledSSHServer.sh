03776aac65d81a1c852fcc3d11036803e3bf26de
djq <anotherme@aliyun.com>
2024-03-29 16:09:42 +0800

    优化脚本语法

--- enabledSSHServer.sh
+++ enabledSSHServer.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 addConfig(){
 #判断 file.sh 文件中是否存在该字符串
-echo '#'"$1"
-if grep '#'"$1" /etc/ssh/sshd_config  >/dev/null
+echo "param:${1}"
+if grep "#${1}" /etc/ssh/sshd_config  >/dev/null
 then
 #存在注释，则替换
-   echo "replace file"
-   sed -i 's/#'"$1"'/'"$1"'/g' /etc/ssh/sshd_config
-elif ! grep "$1" /etc/ssh/sshd_config  >/dev/null
+   echo "replace file:#${1} to ${1}"
+   sed -i "s/#${1}/${1}/g" /etc/ssh/sshd_config
+elif ! grep ${1} /etc/ssh/sshd_config  >/dev/null
 then
 #不存在，添加字符串
-   echo "input file"
-   sed -i '$a'"$1"'' /etc/ssh/sshd_config
+   echo "input file:${1}"
+   sed -i "$a${1}" /etc/ssh/sshd_config
 else
 #存在，不做处理
    echo "not input file"
@@ -19,12 +19,12 @@ fi
 }
 replaceConfig(){
 #判断 file.sh 文件中是否存在该字符串
-echo '#'"$1"
-if grep '#'"$1" /etc/ssh/sshd_config  >/dev/null
+echo "param:${1}"
+if grep "#${1}" /etc/ssh/sshd_config  >/dev/null
 then
 #存在注释，则替换
-   echo "replace file"
-   sed -i 's/#'"$1"'/'"$1"'/g' /etc/ssh/sshd_config
+   echo "replace file:#${1} to ${1}"
+   sed -i "s/#${1}/${1}/g" /etc/ssh/sshd_config
 else
 #存在，不做处理
    echo "not input file"
