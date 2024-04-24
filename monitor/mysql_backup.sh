f9aada785998dfb7b26ce54bc73938b502314b94
djq <anotherme@aliyun.com>
2024-04-24 17:44:05 +0800

    增加钱包导入功能

--- monitor/mysql_backup.sh
+++ monitor/mysql_backup.sh
@@ -1,1 +1,5 @@
-docker exec -it mysql mysqldump --default-character-set=utf8mb4 --single-transaction -uroot -pens_search2022 --all-databases > /data/docker/mysql-back/alldatabases.sql
+docker exec -it mysql mysqldump --default-character-set=utf8mb4 --single-transaction -uroot -pens_search2022 --all-databases > /data/docker/mysql-back/backups/alldatabases.sql
+cd /data/docker/mysql-back/backups
+git add alldatabases.sql
+git commit -m "数据库备份"
+git push -u origin "master"
