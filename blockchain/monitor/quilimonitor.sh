76b0976910d1bf7ed6dc38d2a75d1db2969ab259
flagDai <flagdai@gmail.com>
2024-09-08 03:26:55 +0800

    监控quili

---
+++ blockchain/monitor/quilimonitor.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+url='204.12.203.253:82'
+OLDIFS="$IFS"
+IFS=$'\n'
+for line in $(echo 6|./Quili.sh|grep -E 'Version|Unclaimed balance');do
+	IFS=':'
+	array=(${line})
+	echo "${array[0]}""${array[1]}"
+	if [ "${array[0]}" == "Version" ]
+	then
+		version=`echo ${array[1]}| xargs`
+	elif [ "${array[0]}" == "Unclaimed balance" ]
+	then
+		balance=`echo ${array[1]/ QUIL/}| xargs`
+	fi
+done
+IFS="$OLD_IFS"
+path=$url'/web_crawler/eth/node/updateQuiliBalance?nodeName='$nodeName'&version='$version'&balance='$balance
+echo 'curl '$path
+curl $path
