edddae22d72bcc4d28cd2f715117682b5696fc55
flagDai <flagdai@gmail.com>
2024-09-26 11:43:02 +0800

    备份

--- blockchain/monitor/quilimonitor.sh
+++ blockchain/monitor/quilimonitor.sh
@@ -42,38 +42,49 @@ log 'curl '$path1
 result=$(curl $path1)
 log $result
 isBackup=$(echo $result|grep -oP '"isBackup":(true|false)'| awk -F':' '{print $2}')
+isBackup=$(echo $result|grep -oP '"isBackup":(true|false)'| awk -F':' '{print $2}')
 log 'isBackup:'$isBackup
 if [[ "$isBackup" == "true" ]]; then
     log "开始备份"
+    mkdir -p /root/backup
+    tar -czvf  /root/backup/config.tar.gz /root/ceremonyclient/node/.config
     ip=$(echo $result|grep -oP '"ip":\K[^,}]*'| sed 's/"//g')
     user=$(echo $result|grep -oP '"user":\K[^,}]*'| sed 's/"//g')
     password=$(echo $result|grep -oP '"password":\K[^,}]*'| sed 's/"//g')
     filePath=$(echo $result|grep -oP '"filePath":\K[^,}]*'| sed 's/"//g')
-    scpCommand="scp -v -P 22 -r /root/backup/config.tar.gz $user@$ip:$filePath/$nodeName"
-    log $scpCommand
+    sshCommand="ssh -p 22 -o StrictHostKeyChecking=no \"$user@$ip\" \"mkdir -p \\\"$filePath/$nodeName\\\"\""
+    scpCommand="scp -P 22 -o StrictHostKeyChecking=no -r /root/backup/config.tar.gz $user@$ip:$filePath/$nodeName"
+    log $sshCommand
     log $password
-    expect -c "
+    expect <<EOF
         set timeout 30
-        spawn $scpCommand
-        expect \"Are you sure you want to continue connecting\" {
-            send \"yes\r\"
-        }
-        expect \"password:\" {
-            send \"$password\r\"
-        }
-        expect \"Exit status 0\" {
-            exec bash -c \"curl $monitorUrl/web_crawler/eth/node/finishBackup?nodeName=$nodeName\"
-        }
+        spawn $sshCommand
         expect {
-            timeout {
-                puts \"Error: SCP command timed out.\"
-            }
-            eof {
-                puts \"Error: SCP command failed unexpectedly.\"
-            }
+        	"*password:" {
+            send "$password\r"
+            puts "#####send password"
+        	}
         }
         expect eof
-    "
+EOF
+    log $scpCommand
+    log $password
+    expect <<EOF
+        set timeout 30
+        spawn $scpCommand
+        expect {
+        	"*password:" {
+            send "$password\r"
+            puts "#####send password"
+            exp_continue
+        	}
+        }
+        expect "*100%*" {
+            puts "#####finishBackup"
+            exec bash -c "curl $monitorUrl/web_crawler/eth/node/finishBackup?nodeName=$nodeName"
+        }
+        expect eof
+EOF
     log "结束备份"
 else
     log "不用备份"
