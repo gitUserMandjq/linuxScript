b08c52fdb9038fa8fb662d734f6e4d7de51b8b55
flagDai <flagdai@gmail.com>
2024-10-01 20:00:06 +0800

    timeTaken

--- blockchain/monitor/quilimonitor.sh
+++ blockchain/monitor/quilimonitor.sh
@@ -21,6 +21,7 @@ done
 
 FILE_PATH="/var/log/quili.log"
 increment=$(tail -n 100 $FILE_PATH|grep -a 'increment'|tail -n 1|grep -oP '"increment":[0-9]+'| awk -F':' '{print $2}')
+time_taken=$(tail -n 100 $FILE_PATH|grep -a 'time_taken'|tail -n 1|grep -oP '"time_taken":\K[^,}]*')
 # 获取文件大小（以字节为单位）
 FILESIZE=$(stat -c %s "$FILE_PATH")
 log "日志文件大小为: $(echo "scale=2; $FILESIZE / (1024 * 1024)" | bc) MB"
@@ -33,7 +34,7 @@ else
     log "日志文件小于或等于10MB，保留: $FILE_PATH"
 fi
 IFS="$OLD_IFS"
-path=$monitorUrl'/web_crawler/eth/node/updateQuiliBalance?nodeName='$nodeName'&version='$version'&balance='$balance'&increment='$increment
+path=$monitorUrl'/web_crawler/eth/node/updateQuiliBalance?nodeName='$nodeName'&version='$version'&balance='$balance'&increment='$increment'&time_taken='$time_taken
 log 'curl '$path
 curl $path
 #自动备份
